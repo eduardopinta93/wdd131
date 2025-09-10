9b9965c5b67ca6c52c2224e3b193679ac28b12d2
eduardopinta93 <eduardo.pintam@gmail.com>
2025-09-11 00:21:27 +0200

    Picture Album

---
+++ week02/scripts/temples.js
@@ -0,0 +1,18 @@
+const yearElement = document.getElementById("currentyear");
+const currentyear = new Date().getFullYear();
+yearElement.textContent = currentyear;
+
+const lastModifiedElement = document.querySelector(".lastModified");
+let modifiedDate = document.lastModified;
+lastModifiedElement.textContent = `Last modified: ${modifiedDate}`;
+
+const hamburguerBtn = document.getElementById("hamburguer");
+const navMenu = document.getElementById("primary-nav");
+
+hamburguerBtn.addEventListener("click", () =>{
+    navMenu.classList.toggle("open");
+    const isOpen = navMenu.classList.contains("open");
+    hamburguerBtn.textContent = isOpen ? "✕" : "☰"
+    hamburguerBtn.setAttribute("aria-expanded", isOpen);
+    document.querySelector("header p").style.display = isOpen ? "none" : "block";
+});
